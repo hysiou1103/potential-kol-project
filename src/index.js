40a072843e52d4b3320f926fd13017cbb92a62f4
molly <hysiou1103@gmail.com>
2022-05-06 19:28:43 +0800

    [Fix] Router issue

--- src/index.js
+++ src/index.js
@@ -8,10 +8,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-        <App />
+    <BrowserRouter basename='potential-kol-project'>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
+
 );
 
 reportWebVitals();
